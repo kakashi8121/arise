3a3f8ab4f889c65a8c69fff82a7ce1fe5c06869e
SRIHAREE <sriharee21@gmail.com>
2026-07-23 12:29:13 -0700

    Create ARISE design system

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF6C5CE7);
+  static const Color secondary = Color(0xFF00D4FF);
+
+  static const Color background = Color(0xFF0B0F19);
+
+  static const Color surface = Color(0xFF161B26);
+
+  static const Color success = Color(0xFF2ECC71);
+
+  static const Color warning = Color(0xFFF39C12);
+
+  static const Color danger = Color(0xFFE74C3C);
+
+  static const Color textPrimary = Colors.white;
+
+  static const Color textSecondary = Color(0xFFB7BDC9);
+}
